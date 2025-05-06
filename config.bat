1963e5f8a4dbca43a34405c5a723c8eb69724684
Akshat Kumar <47633507+imakshat47@users.noreply.github.com>
2025-05-06 13:06:58 +0530

    Automated Git sync script

---
+++ config.bat
@@ -0,0 +1,7 @@
+:: config.bat
+set "rootPath=C:\Users\Imak47\Documents"
+set "deviceName=LabSystem"
+set "username=ImAkshat47"
+set "defaultCommitMsg=Update from %deviceName% by %username%"
+set "debugMode=false"
+set "enableShutdown=false"
